32d08b33f7bcf44c6cfa5b9e2cc52bee7b25a317
Th3darkcatYTFR <owen.menchero@bts-malraux.net>
2021-12-26 20:22:37 +0100

    Update Start and Stop Command

--- bot.py
+++ bot.py
@@ -12,15 +12,34 @@ def main():
 
 
     activity = nextcord.Activity(type=nextcord.ActivityType.watching, name="le futur")
-    bot = commands.Bot(command_prefix="./", activity = activity, status=nextcord.Status.idle,help_command = None)
+    bot = commands.Bot(command_prefix="/", activity = activity, status=nextcord.Status.idle,help_command = None)
 
     load_dotenv()
 
     @bot.event
     async def on_ready():
         print(f"{bot.user.name} est désormais connecté a discord avec succès !")
+        bot.startGame = False
 
-            #Identification du systeme de connexion
+    # Démarrage du jeu
+    @bot.command
+    async def start(ctx):
+        if bot.startGame == False:
+            bot.startGame = True
+            ctx.message.send("Que le jeu commence !")
+        else:
+            ctx.message.send("Le jeu est déjà en cours !")
+
+    # Arret du jeu
+    @bot.command
+    async def stopgame(ctx):
+        if bot.startGame == True:
+            bot.starGame = False
+            ctx.message.send("Arret du jeu !")
+        else:
+            ctx.message.send("Aucun jeu en cours !")
+
+    #Identification du systeme de connexion
     bot.run(os.getenv("DISCORD_TOKEN"))
 
 
