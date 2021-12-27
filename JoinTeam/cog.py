0d9b7dbe644b2f9988693afe39a843478492e986
Th3darkcatYTFR <owen.menchero@bts-malraux.net>
2021-12-27 22:14:47 +0100

    Update because Error

--- JoinTeam/cog.py
+++ JoinTeam/cog.py
@@ -1,18 +1,19 @@
 from .role_view import RoleView
 from nextcord.ext import commands
 
-from JoinTeam.role_view import RoleView
-
 class ButtonRoles(commands.Cog, name = "Roles Main"):
 
     def __init__(self, bot: commands.Bot):
-        self.bot = bot
+        self.__bot = bot
 
     @commands.Cog.listener()
     async def on_ready(self):
-        self.bot.add_view(RoleView())
+        self.__bot.add_view(RoleView())
 
     @commands.command()
     @commands.has_permissions(manage_message = True)
     async def roles(self, ctx: commands.Context):
         await ctx.send("Choisir votre équipe en cliquant sur le bouton de votre choix !", view = RoleView())
+
+def setup(bot):
+    bot.add_cog(ButtonRoles(bot))
--- config.py
+++ config.py
@@ -9,4 +9,4 @@ TEAM_RED_ID = int(os.getenv("TEAM_RED_ID",""))
 TEAM_BLUE_ID = int(os.getenv("TEAM_BLUE_ID",""))
 NO_TEAM_ID = int(os.getenv("NO_TEAM_ID",""))
 
-TOKEN_BOT = int(os.getenv("DISCORD_TOKEN",""))
+TOKEN_BOT = os.getenv("DISCORD_TOKEN","")
