aacf6b8a72d73836ba80afe6127ff2b3cf892a14
Th3darkcatYTFR <owen.menchero@bts-malraux.net>
2021-12-27 23:40:34 +0100

    Upd

--- JoinTeam/role_view.py
+++ JoinTeam/role_view.py
@@ -20,10 +20,10 @@ class RoleView(nextcord.ui.View):
             await interaction.user.add_roles(role)
             await interaction.response.send_message("Add to Team", ephemeral = True)
 
-    @nextcord.ui.button(label = "Equipe rouge", emoji = "RED:925135933932732496", style = nextcord.ButtonStyle.red, custom_id = custom_id(VIEW_NAME, config.TEAM_RED_ID))
+    @nextcord.ui.button(label = "Equipe rouge", emoji = "📕", style = nextcord.ButtonStyle.red, custom_id = custom_id(VIEW_NAME, config.TEAM_RED_ID))
     async def red_button(self, button, interaction):
         await self.handle_click(button, interaction)
 
-    @nextcord.ui.button(label = "Equipe bleue", emoji = "BLUE:925135962835681341", style = nextcord.ButtonStyle.primary, custom_id = custom_id(VIEW_NAME, config.TEAM_BLUE_ID))
+    @nextcord.ui.button(label = "Equipe bleue", emoji = "📘", style = nextcord.ButtonStyle.primary, custom_id = custom_id(VIEW_NAME, config.TEAM_BLUE_ID))
     async def blue_button(self, button, interaction):
         await self.handle_click(button, interaction)
