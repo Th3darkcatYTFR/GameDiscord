61c685142c6e6b22df55e5a4f041213b11af2a3d
Th3darkcatYTFR <owen.menchero@bts-malraux.net>
2021-12-27 22:16:36 +0100

    Up same

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ load_dotenv
 
 PREFIX = "/"
 BOT_NAME = "Duel Bot - Game"
-TEAM_RED_ID = int(os.getenv("TEAM_RED_ID",""))
-TEAM_BLUE_ID = int(os.getenv("TEAM_BLUE_ID",""))
-NO_TEAM_ID = int(os.getenv("NO_TEAM_ID",""))
+TEAM_RED_ID = os.getenv("TEAM_RED_ID","")
+TEAM_BLUE_ID = os.getenv("TEAM_BLUE_ID","")
+NO_TEAM_ID = os.getenv("NO_TEAM_ID","")
 
 TOKEN_BOT = os.getenv("DISCORD_TOKEN","")
