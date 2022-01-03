b5f77733766e0a73ae149835d5c24597ed696982
Th3darkcatYTFR <owen.menchero@bts-malraux.net>
2022-01-03 22:31:04 +0100

    up

--- bot.py
+++ bot.py
@@ -1,30 +1,23 @@
-import discord
-import random
+from discord import Client, Intents, Embed
+from discord_slash import SlashCommand, SlashContext
 import config
-from discord.ext import commands
-from discord_slash import SlashCommand
 from dotenv.main import load_dotenv
 
 
 def main():
 
-
-
-
-    bot = commands.Bot(command_prefix = "k!", description = "Test")
-    slash = SlashCommand(bot, sync_commands = True)
+    bot = Client(intents=Intents.default())
+    slash = SlashCommand(bot)
     load_dotenv()
 
     @bot.event
     async def on_ready():
-        print("Kurama est maintenant connecté !")
+        print("Mon Bot est maintenant connecté !")
 
-    @slash.slash(name="help", description="Affiche la liste des commandes disponible !")
-    async def helping(ctx):
-        embed = discord.Embed(description = "Liste des commandes !", url = "https://cdn.discordapp.com/avatars/833726936890867784/35a4505aa389a112a73b3dbd4051f78e.png", color=0xFF7D42)
-        embed.add_field(name = "Fun", value = "`funfact`")
-
-        await ctx.send(embed)
+    @slash.slash(name="test")
+    async def test(ctx: SlashContext):
+        embed = Embed(title="Embed Test")
+        await ctx.send(embed=embed)
 
     bot.run(config.TOKEN_BOT)
 
